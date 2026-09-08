e1b8f706bf7edf12cb0f814bcf068d1ece471a1d
devivaraprasad <vudidevivaraprasad143@gmail.com>
2026-09-08 20:10:56 +0530

    add download resume functionality to navbar with async pdf retrieval

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,14 +1,18 @@
-import { Component, effect, input, output, signal } from '@angular/core';
+import { Component, effect, inject, input, output, signal } from '@angular/core';
+import { Contentful } from '../../services/contentful';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   scrolledToNavItem = input.required<string>();
   selectedNavItem = output<string>();
+  contentfulService = inject(Contentful);
   NavItems = signal<string[]>(['Home','Skills','Projects','Experience','About'])
   selected = signal<string>(this.NavItems()[0])
 
@@ -16,6 +20,19 @@ export class Navbar {
     effect(() => {
       this.selected.set(this.scrolledToNavItem())
     })
+  }
+
+  private readonly pdfRequests = new Map<string, Observable<string>>();
+
+  getpdf(id: string): Observable<string> {
+    let pdfRequest = this.pdfRequests.get(id);
+
+    if (!pdfRequest) {
+      pdfRequest = this.contentfulService.getAssetBasedonId(id);
+      this.pdfRequests.set(id, pdfRequest);
+    }
+
+    return pdfRequest;
   }
 
   navItemClick(item:string){
